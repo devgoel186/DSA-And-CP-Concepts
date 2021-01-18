8bbc6556a1919fc49b9ac8412f23117f4b5c96e1
Dev Goel <goeldev01@gmail.com>
2021-01-18 19:23:35 +0530

    Added List midpoint code

---
+++ Coding Ninjas C++/LinkedList/List_midpoint.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+#include "./Node.cpp"
+
+Node *takeInputWithTail()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+Node *findMidpoint(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = (fast->next)->next;
+    }
+    return slow;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << endl;
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node *head = takeInputWithTail();
+
+    Node *midpoint = findMidpoint(head);
+    cout << "Midpoint - " << midpoint->data << endl;
+
+    cout << "\nThe Linked List is :" << endl;
+    print(head);
+}
