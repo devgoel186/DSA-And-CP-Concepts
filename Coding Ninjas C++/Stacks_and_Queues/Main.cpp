6a00237634065213150da2bbd101d4e8091d9be7
Dev Goel <goeldev01@gmail.com>
2021-01-20 22:51:20 +0530

    Added stacks using dynamic arrays

--- Coding Ninjas C++/Stacks_and_Queues/Main.cpp
+++ Coding Ninjas C++/Stacks_and_Queues/Main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
-#include "./StackUsingArray.cpp"
+#include "./StackUsingDynamicArray.cpp"
 
 int main()
 {
     // Creating stack
-    StackUsingArray s(4);
+    StackUsingArray s;
     s.push(10);
     s.push(20);
     s.push(30);
     s.push(40);
     s.push(50);
+    s.push(60);
+    s.push(25);
+    s.push(40);
 
     // top() function
     cout << s.top() << endl;
 
     // pop() operations
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
     cout << s.pop() << endl;
     cout << s.pop() << endl;
     cout << s.pop() << endl;
