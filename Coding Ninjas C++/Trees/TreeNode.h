3950a80ec18b21349a2233f7ba0b3755350849dd
Dev Goel <goeldev01@gmail.com>
2021-01-23 00:15:04 +0530

    Added trees input and print using vectors

---
+++ Coding Ninjas C++/Trees/TreeNode.h
@@ -0,0 +1,16 @@
+// #include <iostream>
+// using namespace std;
+#include <vector>
+
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
