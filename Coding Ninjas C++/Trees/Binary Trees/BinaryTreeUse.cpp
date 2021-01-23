410c4d621e1bf9bf8a9b5755ab75f47e6a968a69
Dev Goel <goeldev01@gmail.com>
2021-01-24 02:02:33 +0530

    Added tree processing code ; input, print, and levelwise input

--- Coding Ninjas C++/Trees/Binary Trees/BinaryTreeUse.cpp
+++ Coding Ninjas C++/Trees/Binary Trees/BinaryTreeUse.cpp
@@ -17,15 +17,71 @@ void printTree(BinaryTreeNode<int> *root)
     printTree(root->right);
 }
 
+BinaryTreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+        return NULL;
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    BinaryTreeNode<int> *leftChild = takeInput();
+    BinaryTreeNode<int> *rightChild = takeInput();
+
+    root->left = leftChild;
+    root->right = rightChild;
+    return root;
+}
+
+BinaryTreeNode<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+        return NULL;
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+
+        cout << "Enter left child of " << front->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+
+        cout << "Enter right child of " << front->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+        if (rightChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
 int main()
 {
-    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
-    BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
-    BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
-
-    root->left = node1;
-    root->right = node2;
-
+    // BinaryTreeNode<int> *root = takeInput();
+    BinaryTreeNode<int> *root = takeInputLevelWise();
+    // BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
+    // BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
+    // root->left = node1;
+    // root->right = node2;
     printTree(root);
     delete root;
 }
